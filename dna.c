197c85aa802585232c326d29ec5473567f05a33f
misha <shevel55505@gmail.com>
2020-06-12 22:09:12 +0300

    2nd commit

--- dna.c
+++ dna.c
@@ -7,12 +7,21 @@ typedef enum {A=0, C=1, G=2, T=3} Nucleic_acid;
 
 /******************************************************************************/
 Nucleic_acid toNA(unsigned char c) {
-	// YOUR CODE HERE
-}
+    if(c!=A &&  c!=C && c!=G && c!=T) {
+        printf("Gene Format Error\n");
+        exit(1);
+    }
+    return (Nucleic_acid) c;
+    }
+
 
 /******************************************************************************/
 unsigned char toChar(Nucleic_acid c) {
-	// YOUR CODE HERE
+	if(c!=0 &&  c!=1 && c!=2 && c!=3 ){
+	    printf("Undefined Value Error\n");
+	    exit(1);
+	}
+    return (unsigned char) c;
 }
 
 /******************************************************************************/
